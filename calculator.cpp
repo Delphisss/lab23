4a6138300c0458b548deb3d02d9f0d5bc0fc83b3
D-Delphis <kong_led_@hotmail.com>
2023-03-05 15:10:24 +0700

    20%

--- calculator.cpp
+++ calculator.cpp
@@ -1,19 +1,29 @@
 #include <windows.h>
 
+LRESULT CALLBACK WndProc(HWND,UINT , WPARAM ,LPARAM);
 /* This is where all the input to the window goes to */
+char Text1[20];
+char Text2[20];
+HWND Text_Field,Button,Text_Box1,Text_Box2;
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
 	switch(Message) {
-		
-		/* Upon destruction, tell the main thread to stop */
-		case WM_DESTROY: {
-			PostQuitMessage(0);
-			break;
+			case WM_CREATE: 
+				Text_Field = CreateWindow("STATIC","   Please input two numbers",WS_VISIBLE|WS_CHILD|WS_BORDER,20,20,200,25,hwnd,NULL,NULL,NULL);
+				Text_Box1 = CreateWindow("EDIT"," ",WS_BORDER|WS_CHILD|WS_VISIBLE,50,50,150,25,hwnd,NULL,NULL,NULL);
+				Text_Box2 = CreateWindow("EDIT"," ",WS_BORDER|WS_CHILD|WS_VISIBLE,50,80,150,25,hwnd,NULL,NULL,NULL);
+				Button = CreateWindow("BUTTON","+",WS_VISIBLE|WS_CHILD|WS_BORDER,64,120,28,25,hwnd,(HMENU) 1,NULL,NULL);
+				Button = CreateWindow("BUTTON","-",WS_VISIBLE|WS_CHILD|WS_BORDER,94,120,28,25,hwnd,(HMENU) 2,NULL,NULL);
+				Button = CreateWindow("BUTTON","*",WS_VISIBLE|WS_CHILD|WS_BORDER,124,120,28,25,hwnd,(HMENU) 3,NULL,NULL);
+				Button = CreateWindow("BUTTON","/",WS_VISIBLE|WS_CHILD|WS_BORDER,154,120,28,25,hwnd,(HMENU) 4,NULL,NULL);
+				break;
+			
+			case WM_DESTROY:
+				PostQuitMessage(0);
+				break;
+			default:
+				return DefWindowProc (hwnd,Message,wParam,lParam);
 		}
-		
-		/* All other messages (a lot of them) are processed using default procedures */
-		default:
-			return DefWindowProc(hwnd, Message, wParam, lParam);
-	}
 	return 0;
 }
 
@@ -41,17 +51,22 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 0;
 	}
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Caption",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
+	hwnd = CreateWindowEx(
+		WS_EX_CLIENTEDGE,
+		"WindowClass",
+		"My Calculator",//ชื่อ
+		WS_VISIBLE|WS_SYSMENU, // เปลี่ยนจาก WS_VISIBLE|WS_OVERLAPPEDWINDOW เพราะไม่ให้มันปรับย่อหน้าต่างได้
 		CW_USEDEFAULT, /* x */
 		CW_USEDEFAULT, /* y */
-		640, /* width */
-		480, /* height */
-		NULL,NULL,hInstance,NULL);
+		250, /* width อ.กำหนดมา*/ 
+		200, /* height อ.กำหนดมา*/
+		NULL, NULL, hInstance, NULL);
 
 	if(hwnd == NULL) {
 		MessageBox(NULL, "Window Creation Failed!","Error!",MB_ICONEXCLAMATION|MB_OK);
 		return 0;
 	}
+
 
 	/*
 		This is the heart of our program where all input is processed and 
